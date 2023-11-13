911697bbf85374252ff7a647fecbbae226f6a2e1
ylf456 <145236890+ylf456@users.noreply.github.com>
2023-11-12 23:25:09 -0800

    added overall layout by HTML and CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,79 @@
 var weatherForecastURL;
 var APIkey = 'e60fc08a2453ae74640f703fe37c9c29'
+var getSearchButton = document.getElementById("search-button-id")
+var getsearchBarInput = document.getElementById("search-bar-input");
 
 // sample url = https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 // https://api.openweathermap.org/data/2.5/weather?q={city name},{country code}&appid={API key}
 // https://api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}
 // https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
-//geocode convert api : 
-var LosAngeles = "https://api.openweathermap.org/data/2.5/weather?q=Los Angeles&appid=" + APIkey
+//geocode convert api : http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+var LosAngeles = "https://api.openweathermap.org/data/2.5/weather?q=los angeles&appid=" + APIkey
 
-var getWeattherForecastByCityName = function (weatherForecastURL) {
+var RequestWeatherForecastByCityName = function (weatherForecastURL) {
     fetch(weatherForecastURL)
         .then(function (response) {
             if (response.ok === true) {
-                
                 console.log(response);
                 return response.json()
-                // call a function to render content to the page
             } else {
                 alert("Error:" + response.statusText);
             }
         })
-        .then(function(data){
+        .then(function (data) {
             console.log(data);
             console.log(data.coord)
         }
         )
         .catch(function (error) {
             alert("error message:" + error);
+            console.log("error message" + error);
         });
+}
 
+
+
+
+
+getSearchButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (getsearchBarInput !== null) {
+        var getCityNameFromInput = getsearchBarInput.textContent;
+        getsearchBarInput.textContent = "";
+        assembleApiRequsetURL(getCityNameFromInput);
+    } else if (getsearchBarInput.textContent === null) { 
+        alert("please enter a city name!");
+    return; }
+}
+)
+
+var assembleApiRequsetURL = function (cityName) {
+   var assembledURLforTodaysWeather = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName  + "&appid=" + APIkey
+   var assembledURLforFiveDayForecast = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName +"&appid=" + APIkey
+   RequestCityNameThruGeocode(assembledURLforFiveDayForecast);
 
 }
 
-getWeattherForecastByCityName(LosAngeles);
+var RequestCityNameThruGeocode = function (GeoCodeData) {
+    fetch(GeoCodeData)
+        .then(function (response) {
+            if (response.ok === true) {
+                console.log(response);
+                return response.json()
+            } else {
+                alert("Error:" + response.statusText);
+            }
+        })
+        .then(function (data) {
+            console.log(data);
+        }
+        )
+        .catch(function (error) {
+            alert("error message:" + error);
+            console.log("error message" + error);
+        });
+}
+
+var location = LosAngeles
+assembleApiRequsetURL(location);
